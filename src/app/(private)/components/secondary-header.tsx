a67d805a864e62e9825fa507f3ece0e33a309822
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-05-20 18:17:25 -0300

    fix: replace Link with button for back navigation in SecondaryHeader

--- src/app/(private)/components/secondary-header.tsx
+++ src/app/(private)/components/secondary-header.tsx
@@ -1,22 +1,26 @@
+'use client'
+
 import { ChevronLeft } from "lucide-react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface SecondaryHeaderProps {
   title: string;
 }
 
 export function SecondaryHeader({ title }: SecondaryHeaderProps) {
+  const router = useRouter();
   return (
     <>
       <header className="p-4 pt-6 fixed top-0 flex items-center w-full justify-center max-w-md mx-auto z-50 bg-background text-white h-16">
-        <Link
-          href="/"
+        <button
+          type="button"
+          onClick={() => router.back()}
           className="mr-4 absolute left-4 flex items-center h-full"
         >
           <span className="flex items-center justify-center bg-muted rounded-full w-12 h-12">
             <ChevronLeft className="h-7 w-7" />
           </span>
-        </Link>
+        </button>
         <h1 className="text-xl font-medium w-full text-center flex items-center justify-center h-full">
           {title}
         </h1>
