0c4f3ef797a58e4e1c3d2ba8949c7ae63adfc5c7
Patrick Morais <ppternunes@gmail.com>
2025-06-17 09:25:54 -0300

    feat(icon-button): create component

---
+++ src/components/ui/custom/icon-button.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { LucideIcon } from 'lucide-react'
+import React from 'react'
+
+interface IconButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon: LucideIcon
+}
+
+export function IconButton({
+  icon: Icon,
+  className,
+  ...props
+}: IconButtonProps) {
+  return (
+    <button
+      type="button"
+      className={cn(
+        'mr-4 absolute left-4 flex items-center h-full cursor-pointer',
+        className
+      )}
+      {...props}
+    >
+      <span className="flex items-center justify-center bg-card rounded-full w-12 h-12">
+        <Icon className="h-5 w-5 text-foreground" />
+      </span>
+    </button>
+  )
+}
