7701701da2d1d1f749347bfdf7c5a8b2aa486fe4
Patrick Morais <ppternunes@gmail.com>
2025-06-26 20:15:16 -0300

    feat(social-name-drawer-content): create component

---
+++ src/app/(private)/components/drawer-contents/social-name-drawer-content.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export function SocialNameDrawerContent() {
+  return (
+    <div className="py-6 px-2 flex justify-center">
+      <p className="text-popover-foreground">
+        {' '}
+        Nome social é o nome que você usa no dia a dia. Ele será respeitado e
+        usado nas comunicações sempre que possível.{' '}
+      </p>
+    </div>
+  )
+}
