1d8bacca706a4fa0a2be20722cb8b1ea0d964de5
Patrick Morais <ppternunes@gmail.com>
2025-07-03 12:46:09 -0300

    ref: change chevron-left on secondary-header

--- src/app/(private)/components/secondary-header.tsx
+++ src/app/(private)/components/secondary-header.tsx
@@ -1,7 +1,7 @@
 'use client'
 
+import { ChevronLeftIcon } from '@/assets/icons/chevron-left-icon'
 import { IconButton } from '@/components/ui/custom/icon-button'
-import { ChevronLeft } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 
 interface SecondaryHeaderProps {
@@ -14,7 +14,7 @@ export function SecondaryHeader({ title }: SecondaryHeaderProps) {
   return (
     <>
       <header className="p-4 pt-6 fixed top-0 flex items-center w-full justify-center max-w-md mx-auto z-50 bg-background text-foreground h-16">
-        <IconButton icon={ChevronLeft} onClick={() => router.back()} />
+        <IconButton icon={ChevronLeftIcon} onClick={() => router.back()} />
         <h1 className="text-xl font-medium w-full text-center flex items-center justify-center h-full text-foreground">
           {title}
         </h1>
