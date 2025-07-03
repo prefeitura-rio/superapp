df17adc57af4770f03e71e297f38e9685971a4c9
Patrick Morais <ppternunes@gmail.com>
2025-07-03 12:47:04 -0300

    ref: change x and check circle-icon on input-field

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,25 @@
+import { CheckCircleIcon } from './check-circle-icon'
+import { ChevronLeftIcon } from './chevron-left-icon'
+import { ChevronRightIcon } from './chevron-right-icon'
+import { EditIcon } from './edit-icon'
+import { HomeIcon } from './home-icon'
+import { InfoIcon } from './info-icon'
+import { SearchIcon } from './search-icon'
+import { ServicesIcon } from './services-icon'
+import { UserIcon } from './user-icon'
+import { WalletIcon } from './wallet-icon'
+import { XCircleIcon } from './x-circle-icon'
+
+export {
+  CheckCircleIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  EditIcon,
+  HomeIcon,
+  InfoIcon,
+  SearchIcon,
+  ServicesIcon,
+  UserIcon,
+  WalletIcon,
+  XCircleIcon,
+}
