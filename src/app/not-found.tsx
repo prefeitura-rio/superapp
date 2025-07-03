dd79e0ac064f6d757eeda8de22ae79d29341a550
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-07-03 18:05:03 -0300

    ref: not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,43 +1,46 @@
 import { CustomButton } from '@/components/ui/custom/custom-button'
 import Link from 'next/link'
+import { SecondaryHeader } from './(private)/components/secondary-header'
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-background">
-      <div className="max-w-md mx-auto text-center px-4">
-        <div className="mb-8">
-          <div
-            className="mb-4 flex items-center justify-center"
-            style={{ width: 350, height: 350 }}
-          >
-            <video
-              src="/3d_test.mp4"
-              width={350}
-              height={350}
-              style={{
-                objectFit: 'contain',
-                width: '100%',
-                height: '100%',
-              }}
-              loop
-              autoPlay
-              muted
-              playsInline
-            />
-          </div>
-          <h1 className="text-6xl font-bold text-primary mb-2">Oops...</h1>
-          <h2 className="text-2xl font-semibold text-foreground mb-4">
-            Página não encontrada
-          </h2>
+    <div className="max-w-md min-h-lvh mx-auto pt-30 flex flex-col">
+      <SecondaryHeader title="" />
+      <div className="px-4">
+        <div
+          className="mb-8 flex items-center justify-center"
+          style={{ height: 250 }}
+        >
+          <video
+            src="/3d_test.mp4"
+            width={350}
+            height={350}
+            style={{
+              objectFit: 'contain',
+              width: '100%',
+              height: '100%',
+            }}
+            loop
+            autoPlay
+            muted
+            playsInline
+          />
         </div>
-
-        <div className="space-y-4">
-          <CustomButton size="xl" variant="primary" fullWidth>
-            <Link href="/">Voltar ao início</Link>
-          </CustomButton>
-
-          <CustomButton size="xl" variant="outline" fullWidth>
-            <Link href="/services">Acessar serviços</Link>
+        <h2 className="text-2xl font-semibold text-foreground mb-8">
+          Página não encontrada
+        </h2>
+        <p className="text-muted-foreground mb-16">
+          Não encontramos a página que você tentou acessar. Por favor, verifique
+          o link ou retorne à pagina inicial.
+        </p>
+        <div className="space-y-4 mb-16">
+          <CustomButton
+            size="xl"
+            className="rounded-full"
+            variant="primary"
+            fullWidth
+          >
+            <Link href="/">Voltar para a Home</Link>
           </CustomButton>
         </div>
       </div>
