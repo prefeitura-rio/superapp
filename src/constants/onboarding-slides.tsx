e6f5090d6b31f4fca45d7f0dc357b2a4ff0202e4
Patrick Morais <ppternunes@gmail.com>
2025-07-09 12:50:20 -0300

    feat(onboarding-slides): create constant

---
+++ src/constants/onboarding-slides.tsx
@@ -0,0 +1,20 @@
+export const onboardingSlides = [
+  {
+    title: 'O Rio inteiro em um só app',
+    description:
+      'Consulte IPTU e ITBI, gere certidões, agende atendimentos e muito mais – sem burocracia.',
+    video: '3d_test.mp4',
+  },
+  {
+    title: 'Serviços municipais a seu alcance',
+    description:
+      'Marque consultas médicas, confira frequência escolar, consulte seus impostos e muito mais.',
+    video: '3d_test.mp4',
+  },
+  {
+    title: 'Sua cidadania na palma da mão',
+    description:
+      'Acesse o Bolsa Família, Cartão Saúde e Zeladoria num só lugar – simples, digital e seguro.',
+    video: '3d_test.mp4',
+  },
+]
