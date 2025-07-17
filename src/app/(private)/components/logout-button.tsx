6de5f3bfd36c96540de24cd2eb916d39e46ce054
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-07-17 18:24:36 -0300

    fix venv

--- src/app/(private)/components/logout-button.tsx
+++ src/app/(private)/components/logout-button.tsx
@@ -8,12 +8,11 @@ export function LogoutButton() {
     // TODO: add redirectUri to govbr
     try {
       // uri de redirecionamento do keycloak
-      const redirectUri =
-        'https://auth-idriohom.apps.rio.gov.br/auth/realms/idrio_cidadao/protocol/openid-connect/auth?client_id=superapp&redirect_uri=https://staging.app.dados.rio/api/auth/callback/keycloak&response_type=code'
+      const redirectUri = `${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_BASE_URL}/auth?client_id=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_REDIRECT_URI}&response_type=code`
       // primeiro faz logout do keycloak
       await fetch('/api/auth/logout')
       // depois faz logout do gov.br via iframe oculto
-      const govbrLogoutUrl = `https://sso.staging.acesso.gov.br/logout?post_logout_redirect_uri=${encodeURIComponent(redirectUri)}`
+      const govbrLogoutUrl = `${process.env.NEXT_PUBLIC_GOVBR_BASE_URL}logout?post_logout_redirect_uri=${encodeURIComponent(redirectUri)}`
       const iframe = document.createElement('iframe')
       iframe.style.display = 'none'
       iframe.src = govbrLogoutUrl
@@ -23,8 +22,7 @@ export function LogoutButton() {
         setTimeout(() => {
           document.body.removeChild(iframe)
           // Redireciona o usuário após o logout do govbr
-          const redirectUrl = `${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_BASE_URL}/auth?client_id=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_REDIRECT_URI}&response_type=code`
-          window.location.href = redirectUrl
+          window.location.href = redirectUri
         }, 0) // coloca em ultima prioridade na stack de execução
       }
     } catch (error) {
