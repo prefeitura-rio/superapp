2561862198d575abcee5e9108aef431d0629b49b
Patrick Morais <ppternunes@gmail.com>
2025-07-23 10:28:20 -0300

    fix(not-found page): fix assets

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,35 +1,29 @@
+'use client'
+
 import { CustomButton } from '@/components/ui/custom/custom-button'
 import Link from 'next/link'
+import { ThemeAwareVideo } from '../components/ui/custom/theme-aware-video'
+import { VIDEO_SOURCES } from '../constants/videos-sources'
+import { useViewportHeight } from '../hooks/useViewportHeight'
 import { SecondaryHeader } from './components/secondary-header'
 
 export default function NotFound() {
+  const { isBelowBreakpoint } = useViewportHeight(648)
+
   return (
-    <div className="max-w-md min-h-lvh mx-auto pt-30 flex flex-col">
+    <div className="max-w-md max-h-lvh mx-auto pt-20 flex flex-col overflow-y-hidden">
       <SecondaryHeader title="" />
       <div className="px-4">
-        <div
-          className="mb-8 flex items-center justify-center"
-          style={{ height: 250 }}
-        >
-          <video
-            src="/3d_test.mp4"
-            width={350}
-            height={350}
-            style={{
-              objectFit: 'contain',
-              width: '100%',
-              height: '100%',
-            }}
-            loop
-            autoPlay
-            muted
-            playsInline
-          />
-        </div>
-        <h2 className="text-2xl font-semibold text-foreground mb-8">
+        <ThemeAwareVideo
+          source={VIDEO_SOURCES.notFound}
+          containerClassName="mb-8 flex items-center justify-center h-[min(328px,50vh)] max-h-[328px]"
+        />
+        <h2 className="text-2xl font-semibold text-foreground mb-1">
           Página não encontrada
         </h2>
-        <p className="text-muted-foreground mb-16">
+        <p
+          className={`text-muted-foreground ${isBelowBreakpoint ? 'mb-6' : 'mb-16'}`}
+        >
           Não encontramos a página que você tentou acessar. Por favor, verifique
           o link ou retorne à pagina inicial.
         </p>
