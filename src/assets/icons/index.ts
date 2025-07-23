224c9db0c5e64a55242abfc5b541e7779d8d3e71
Patrick Morais <ppternunes@gmail.com>
2025-07-23 12:37:00 -0300

    create icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,25 +1,33 @@
+import { CalendarIcon } from './calendar-icon'
 import { CheckCircleIcon } from './check-circle-icon'
 import { ChevronLeftIcon } from './chevron-left-icon'
 import { ChevronRightIcon } from './chevron-right-icon'
 import { EditIcon } from './edit-icon'
 import { HomeIcon } from './home-icon'
 import { InfoIcon } from './info-icon'
+import { MapPinIcon } from './map-pin-icon'
+import { PhoneIcon } from './phone-icon'
 import { SearchIcon } from './search-icon'
 import { ServicesIcon } from './services-icon'
 import { UserIcon } from './user-icon'
 import { WalletIcon } from './wallet-icon'
+import { WhatsappIcon } from './whatsapp-icon'
 import { XCircleIcon } from './x-circle-icon'
 
 export {
+  CalendarIcon,
   CheckCircleIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
   EditIcon,
   HomeIcon,
   InfoIcon,
+  MapPinIcon,
+  PhoneIcon,
   SearchIcon,
   ServicesIcon,
   UserIcon,
   WalletIcon,
+  WhatsappIcon,
   XCircleIcon,
 }
