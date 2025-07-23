bb1141836416241969d74766fa2dd458db162356
Patrick Morais <ppternunes@gmail.com>
2025-07-23 12:41:10 -0300

    feat: add mp4 sources on onboarding

--- src/app/components/onboarding-slider.tsx
+++ src/app/components/onboarding-slider.tsx
@@ -1,3 +1,5 @@
+import { ThemeAwareVideo } from '@/components/ui/custom/theme-aware-video'
+import type { VideoSourceProps } from '@/constants/videos-sources'
 import { forwardRef } from 'react'
 import 'swiper/css'
 import 'swiper/css/pagination'
@@ -5,7 +7,11 @@ import { Pagination } from 'swiper/modules'
 import { Swiper, type SwiperRef, SwiperSlide } from 'swiper/react'
 
 interface OnboardingSliderProps {
-  slides: Array<{ video: string; title: string; description: string }>
+  slides: Array<{
+    videoSource: VideoSourceProps
+    title: string
+    description: string
+  }>
   onSlideChange: (index: number) => void
   isBelowBreakpoint: boolean
 }
@@ -26,62 +32,24 @@ export const OnboardingSlider = forwardRef<SwiperRef, OnboardingSliderProps>(
           {onboardingSlides.map((slide, idx) => (
             <SwiperSlide key={idx}>
               <div
-                className={`flex flex-col items-center justify-center text-left max-h-lvh mt-5 md:mt-0 gap-10 md:gap-18 ${isBelowBreakpoint && 'max-h-[510px]'}`}
+                className={`flex flex-col items-center justify-center text-left max-h-lvh mt-5 md:mt-0 gap-5 md:gap-18 ${isBelowBreakpoint && 'max-h-[410px]'}`}
               >
-                <div
-                  className="mb-4 relative"
-                  style={{
-                    width: 'clamp(160px, 40%, 260px)',
-                    height: 'clamp(200px, 30vh, 460px)',
-                  }}
-                >
-                  <video
-                    src={slide.video}
-                    style={{
-                      objectFit: 'contain',
-                      width: '100%',
-                      height: '100%',
-                    }}
-                    loop
-                    autoPlay
-                    muted
-                    playsInline
-                    onError={e => {
-                      const video = e.currentTarget
-                      const fallback =
-                        video.parentElement?.querySelector('[data-fallback]')
-
-                      if (video && fallback instanceof HTMLElement) {
-                        video.style.display = 'none'
-                        fallback.style.display = 'flex'
-                      }
-                    }}
-                  />
-
-                  {/* Fallback container */}
-                  <div
-                    data-fallback
-                    className="absolute inset-0 bg-gray-200 rounded-lg flex items-center justify-center text-center p-4"
-                    style={{ display: 'none' }}
-                  >
-                    <div>
-                      <div className="w-12 h-12 bg-gray-300 rounded-full mx-auto mb-2 flex items-center justify-center">
-                        <span className="text-gray-500 text-xl ml-1 -mt-1">
-                          ▶
-                        </span>
-                      </div>
-                      <p className="text-gray-500 text-xs">
-                        Vídeo indisponível
-                      </p>
-                    </div>
-                  </div>
-                </div>
+                <ThemeAwareVideo
+                  source={slide.videoSource}
+                  containerClassName={`mb-0 flex items-center justify-center ${
+                    idx === 0 && 'crop-right-5' // Crop right side for first slide video (problematic line on right side)
+                  } ${
+                    isBelowBreakpoint
+                      ? 'h-[min(250px,30vh)] max-h-[250px]'
+                      : 'h-[min(328px,40vh)] max-h-[328px]'
+                  }`}
+                />
 
                 <div className="text-left w-full">
-                  <h2 className="text-4xl text-[#09090B] font-medium mb-2 text-left leading-10 tracking-tight">
+                  <h2 className="text-4xl text-foreground font-medium mb-2 text-left leading-10 tracking-tight">
                     {slide.title}
                   </h2>
-                  <p className="text-left text-[#A1A1A1] text-sm mb-8">
+                  <p className="text-left text-foreground-light text-sm mb-8 leading-5 tracking-normal">
                     {slide.description}
                   </p>
                 </div>
@@ -117,7 +85,7 @@ export const OnboardingSlider = forwardRef<SwiperRef, OnboardingSliderProps>(
             margin: 0 !important;
           }
           .swiper-pagination-bullet-active {
-            background: #09090B;
+            background: var(--foreground);
             width: 24px;
           }
         `}</style>
