87b36724fd87560304a4a55b399af43e92c3b846
Patrick Morais <ppternunes@gmail.com>
2025-08-21 00:57:22 -0300

    feat: add courses card component

--- src/app/components/recommended-courses-cards.tsx
+++ src/app/components/recommended-courses-cards.tsx
@@ -1,6 +1,6 @@
 import type { Course } from '@/types/course'
-import CourseCard from './course-card'
-import { getCourseCardColor, providerIcons } from './utils'
+import { CourseCard } from './courses/courses-card'
+import { providerIcons } from './utils'
 
 interface RecommendedCoursesCardsProps {
   courses: Course[]
@@ -12,27 +12,21 @@ export default function RecommendedCoursesCards({
   const recommended = courses.filter(course => course.recommended)
   return (
     <>
-      <h2 className="text-md font-medium mb-4 px-4 pt-4">Recomendados</h2>
+      <h3 className="pb-2 text-base font-medium text-foreground leading-5 mt-8 px-4">
+        Mais Populares
+      </h3>
       <div className="relative w-full overflow-x-auto pb-2 no-scrollbar">
         <div className="flex gap-4 px-4 w-max">
           {recommended.map(course => (
             <CourseCard
+              courseId={course.id}
               key={course.id}
-              id={course.id}
-              provider={course.provider}
               title={course.title}
-              workload={course.workload}
               modality={course.modality}
-              color={getCourseCardColor(course.type)}
-              icon={providerIcons[course.provider]}
-              status={course.status}
-              date={course.date}
-              type={course.type}
-              recommended={course.recommended}
-              recentlyAdded={course.recentlyAdded}
-              spots={course.spots}
-              description={course.description}
-              requirements={course.requirements}
+              workload={course.workload}
+              providerIcon={providerIcons[course.provider]}
+              provider={course.provider}
+              imageUrl={course.imageUrl}
             />
           ))}
         </div>
