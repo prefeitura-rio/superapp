6cbf3f224fb2a1367119d37c3c73db5c0352f49f
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-08-22 13:41:14 -0300

    chore: remove debg

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -28,7 +28,6 @@ export async function GET(request: Request) {
         },
       }
     )
-    console.log(response)
 
     if (!response.ok) {
       throw new Error('Network response was not ok')
