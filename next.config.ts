97e00818c0ab72ededbffcb31be6dd952aee9576
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-08-29 13:05:54 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,32 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/rj-escritorio-dev-public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'apps.rio.gov.br',
-        port: '',
-        pathname: '/**',
-      },
-       // Unsplash images for course cards mock
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.exemplo.com',
+        hostname: '**',
         port: '',
         pathname: '/**',
       },
