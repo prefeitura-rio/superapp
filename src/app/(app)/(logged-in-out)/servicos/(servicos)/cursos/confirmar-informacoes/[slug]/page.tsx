0a0396e7c5aea99c3ad14ef194b9fe74b6e40dc3
Patrick Morais <ppternunes@gmail.com>
2025-09-02 16:13:45 -0300

    ref: confirm subscription page

--- src/app/(app)/(logged-in-out)/servicos/(servicos)/cursos/confirmar-informacoes/[slug]/page.tsx
+++ src/app/(app)/(logged-in-out)/servicos/(servicos)/cursos/confirmar-informacoes/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import coursesApi from '@/actions/courses'
 import { extractCourseId } from '@/actions/courses/utils-mock'
+import { getDalCitizenCpf } from '@/lib/dal'
+import { getUserInfoFromToken } from '@/lib/user-info'
 import { ConfirmInscriptionClient } from '../components/confirm-inscription-client'
 
 interface PageProps {
@@ -9,16 +11,32 @@ interface PageProps {
 export default async function ConfirmInscriptionPage({ params }: PageProps) {
   const { slug: courseSlug } = await params
   const courseUuid = extractCourseId(courseSlug)
-  const [userInfo, nearbyUnits] = await Promise.all([
-    coursesApi.getUserInfo(),
-    coursesApi.getNearbyUnits(courseUuid),
-  ])
+  const userAuthInfo = await getUserInfoFromToken()
+
+  let userInfoObj = userAuthInfo
+  const promises = [coursesApi.getNearbyUnits(courseUuid)]
+
+  if (userAuthInfo.cpf) {
+    promises.push(getDalCitizenCpf(userAuthInfo.cpf))
+  }
+
+  const results = await Promise.all(promises)
+  const nearbyUnits = results[0]
+  const dataCitizen = results[1]
+
+  if (dataCitizen && dataCitizen.status === 200) {
+    userInfoObj = {
+      ...userAuthInfo,
+      ...dataCitizen.data,
+    }
+  }
 
   return (
     <ConfirmInscriptionClient
-      userInfo={userInfo}
+      userInfo={userInfoObj}
       nearbyUnits={nearbyUnits}
       courseId={courseUuid}
+      courseSlug={courseSlug}
     />
   )
 }
