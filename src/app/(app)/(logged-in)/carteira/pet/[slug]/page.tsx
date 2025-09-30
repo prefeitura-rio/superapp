5eeedb4e51739575c3511f4bc53eb64c66008c12
Patrick Morais <ppternunes@gmail.com>
2025-09-30 13:22:14 -0300

    feat(pet-page): add server page

---
+++ src/app/(app)/(logged-in)/carteira/pet/[slug]/page.tsx
@@ -0,0 +1,88 @@
+import { getEmailValue, normalizeEmailData } from '@/helpers/email-data-helpers'
+import { getPhoneValue, normalizePhoneData } from '@/helpers/phone-data-helpers'
+import {
+  getCitizenCpfPets,
+  getCitizenCpfPetsClinic,
+  getCitizenCpfPetsPetId,
+} from '@/http/citizen/citizen'
+import { getDalCitizenCpf } from '@/lib/dal'
+import { getUserInfoFromToken } from '@/lib/user-info'
+import { PetClientPage } from './pet-client-page'
+import { PetNotFound } from './pet-not-found'
+
+interface PetPageProps {
+  params: {
+    slug: string
+  }
+}
+
+export default async function PetPage({ params }: PetPageProps) {
+  const userAuthInfo = await getUserInfoFromToken()
+  const { slug } = await params
+
+  let dataCitizen = null
+
+  if (userAuthInfo.cpf) {
+    dataCitizen = await getDalCitizenCpf(userAuthInfo.cpf)
+  }
+
+  const userInfoObj = {
+    cpf: userAuthInfo.cpf || '',
+    name: userAuthInfo.name || 'Nome não informado',
+    email: 'Email não cadastrado',
+    phone: 'Telefone não cadastrado',
+  }
+
+  if (dataCitizen?.status === 200) {
+    const emailData = dataCitizen.data?.email || { principal: { valor: '' } }
+    const telefoneData = dataCitizen.data?.telefone || {
+      principal: { ddi: '', ddd: '', valor: '' },
+    }
+
+    userInfoObj.cpf = (dataCitizen.data.cpf as string) || userInfoObj.cpf
+    userInfoObj.name = (dataCitizen.data.nome as string) || userInfoObj.name
+    userInfoObj.email =
+      getEmailValue(normalizeEmailData(emailData)) || userInfoObj.email
+    userInfoObj.phone =
+      getPhoneValue(normalizePhoneData(telefoneData)) || userInfoObj.phone
+  }
+
+  let uniquePet
+  try {
+    uniquePet = await getCitizenCpfPetsPetId(userAuthInfo.cpf, Number(slug))
+  } catch (error) {
+    console.error('Error fetching pet:', error)
+    return <PetNotFound />
+  }
+
+  // fallback if pet not found
+  if (!uniquePet || uniquePet.status !== 200 || !uniquePet.data) {
+    return <PetNotFound />
+  }
+
+  const uniquePetData = uniquePet.data
+
+  const petsResponse = await getCitizenCpfPets(userAuthInfo.cpf)
+  const petsData = petsResponse.data
+
+  const pets =
+    'data' in petsData && Array.isArray(petsData.data) ? petsData.data : []
+  const petHasMicrochip = !!pets[0]?.microchip_numero
+
+  const clinicResponse = await getCitizenCpfPetsClinic(userAuthInfo.cpf)
+  const petsClinic = clinicResponse.data
+
+  let petClinicName = 'Clínica Microchipadora'
+  if ('clinic' in petsClinic) {
+    petClinicName = petsClinic.clinic?.nome as string
+  }
+
+  return (
+    <PetClientPage
+      pet={uniquePetData}
+      petClinicName={petClinicName}
+      petHasMicrochip={petHasMicrochip}
+      tutorInfoObj={userInfoObj}
+    />
+  )
+}
