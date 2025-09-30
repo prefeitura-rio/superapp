0986ca581377b537434f33187bfb1680dcb3e7e5
Patrick Morais <ppternunes@gmail.com>
2025-09-30 13:24:12 -0300

    fix(pet-wallet): pet wallet card

--- src/app/components/wallet-cards/pet-wallet.tsx
+++ src/app/components/wallet-cards/pet-wallet.tsx
@@ -2,6 +2,7 @@
 
 import { PetCardBackContent } from '@/components/ui/custom/pet-card-back-content'
 import { PetCardFrontContent } from '@/components/ui/custom/pet-card-front-content'
+import type { ModelsPet } from '@/http/models'
 import Link from 'next/link'
 import { CardWrapper } from '../card-wrapper'
 import { PetCardBase } from '../pet-card-base'
@@ -11,52 +12,60 @@ interface WalletCardProps {
   enableFlip?: boolean
   asLink?: boolean
   showInitialShine?: boolean
+  petData: ModelsPet
   onClick?: () => void
+}
+
+const sexSerializer: Record<string, string> = {
+  M: 'Macho',
+  F: 'Fêmea',
 }
 
 export function PetCard({
   href,
   enableFlip = true,
+  petData,
   asLink = false,
   showInitialShine = false,
   onClick,
 }: WalletCardProps) {
+  const specieSerialized =
+    petData.especie_nome!.charAt(0).toUpperCase() +
+    petData.especie_nome!.slice(1).toLowerCase()
+
+  const registerDate = petData.registro_data
+    ? new Date(petData.registro_data).toLocaleDateString('pt-BR')
+    : 'Não disponível'
+
+  const birthDate = petData.nascimento_data
+    ? new Date(petData.nascimento_data).toLocaleDateString('pt-BR')
+    : 'Não disponível'
+
   const frontContent = (
     <PetCardBase>
-      {/* <PetCardFrontContent
-        title="REGISTRO GERAL DE ANIMAIS"
-        name={petName}
-        species={species}
-        sex={sex}
-        microchipStatus={microchipStatus}
-        petImageUrl={petImageUrl}
-      /> */}
       <PetCardFrontContent
         title="REGISTRO GERAL DE ANIMAIS"
-        name="Toddysvaldo"
-        species="Canina"
-        sex="Macho"
-        microchipStatus="Pendente"
-        petImageUrl="https://images.unsplash.com/photo-1537151625747-768eb6cf92b2?w=300&h=300&fit=crop&crop=face"
+        name={petData.animal_nome!}
+        species={specieSerialized}
+        sex={sexSerializer[petData.sexo_sigla!] || 'Indefinido'}
+        microchipStatus={
+          petData.microchip_numero
+            ? String(petData.microchip_numero)
+            : 'Pendente'
+        }
+        petImageUrl={petData.foto_url!}
       />
     </PetCardBase>
   )
 
   const backContent = (
     <PetCardBase isBack>
-      {/* <PetCardBackContent
-        birthDate={birthDate}
-        castrated={castrated}
-        registrationDate={registrationDate}
-        communityAnimal={communityAnimal}
-        breed={breed}
-      /> */}
       <PetCardBackContent
-        birthDate="12/04/2013"
-        castrated="Sim"
-        registrationDate="05/09/2023"
-        communityAnimal="Não"
-        breed="SRD (Sem Raça Definida)"
+        birthDate={birthDate}
+        castrated={petData.indicador_castrado ? 'Sim' : 'Não'}
+        registrationDate={registerDate}
+        communityAnimal={'Não disponível'}
+        breed={petData.raca_nome!}
       />
     </PetCardBase>
   )
