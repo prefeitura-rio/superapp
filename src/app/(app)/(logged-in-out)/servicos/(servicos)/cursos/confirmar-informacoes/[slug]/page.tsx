36d57916da4cf55a715e946898a61862da66d521
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-10-21 13:32:29 -0300

    feat: always redirect to govbr idp

--- src/app/(app)/(logged-in-out)/servicos/(servicos)/cursos/confirmar-informacoes/[slug]/page.tsx
+++ src/app/(app)/(logged-in-out)/servicos/(servicos)/cursos/confirmar-informacoes/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { extractCourseId } from '@/actions/courses/utils-mock'
+import { REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE } from '@/constants/url'
 import { normalizeEmailData } from '@/helpers/email-data-helpers'
 import { normalizePhoneData } from '@/helpers/phone-data-helpers'
 import { getApiV1CoursesCourseId } from '@/http-courses/courses/courses'
@@ -9,7 +10,7 @@ import type {
 import { getDalCitizenCpf } from '@/lib/dal'
 import { isUpdatedWithin } from '@/lib/date'
 import { getUserInfoFromToken } from '@/lib/user-info'
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 import { ConfirmInscriptionClient } from '../components/confirm-inscription-client'
 
 interface PageProps {
@@ -23,7 +24,7 @@ export default async function ConfirmInscriptionPage({ params }: PageProps) {
   const userAuthInfo = await getUserInfoFromToken()
 
   if (!userAuthInfo.cpf) {
-    notFound()
+    redirect(REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE)
   }
 
   const [userInfoResponse, courseInfoResponse] = await Promise.all([
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,8 @@
-export const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = `${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_BASE_URL}/auth?client_id=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_REDIRECT_URI}&response_type=code`
+// export const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = `${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_BASE_URL}/auth?client_id=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_REDIRECT_URI}&response_type=code`
+// Essa URL redireciona pra tela de login do Keycloak, com o gif animado
+// Preferi não utilizar a tela de login do Keycloak por uma questão de UX. A opção de ir direto para o gov.br é mais intuitiva.
 
+export const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = `${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_BASE_URL}/auth?client_id=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_REDIRECT_URI}&response_type=code&kc_idp_hint=govbr`
 // URL para redirecionamento direto ao gov.br (bypassing a tela de seleção do Keycloak)
 export const REDIRECT_DIRECT_TO_GOVBR_ROUTE = `${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_BASE_URL}/auth?client_id=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_REDIRECT_URI}&response_type=code&kc_idp_hint=govbr`
 
