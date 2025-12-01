88d5e993cdd7fad2c7a3be7211dd6692826eebfc
Lucas <lucastavarestt@gmail.com>
2025-12-01 08:15:32 -0300

    fix banner skeleton

--- src/app/(app)/(logged-in-out)/(home)/loading.tsx
+++ src/app/(app)/(logged-in-out)/(home)/loading.tsx
@@ -29,7 +29,7 @@ export default function HomeLoading() {
             <div className="flex gap-2 px-4 py-2 w-max">
               {Array.from({ length: 6 }).map((_, i) => (
                 <div key={`mobile-${i}`} className="flex flex-col">
-                  <Skeleton className="w-full h-[104px] rounded-xl min-w-[328px]" />
+                  <Skeleton className="w-full h-[120px] rounded-xl min-w-[328px]" />
                 </div>
               ))}
             </div>
@@ -40,10 +40,10 @@ export default function HomeLoading() {
         <div className="px-4 pb-0 mb-0 overflow-x-hidden mt-2 hidden sm:block">
           <div className="grid grid-cols-2 gap-2">
             <div className="flex flex-col">
-              <Skeleton className="w-full h-[104px] rounded-xl min-w-[328px]" />
+              <Skeleton className="w-full h-[120px] rounded-xl min-w-[328px]" />
             </div>
             <div className="flex flex-col">
-              <Skeleton className="w-full h-[104px] rounded-xl min-w-[328px]" />
+              <Skeleton className="w-full h-[120px] rounded-xl min-w-[328px]" />
             </div>
           </div>
           <div className="justify-center items-center h-12 w-full flex">
