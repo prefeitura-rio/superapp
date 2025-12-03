676e9966993666822f69cb782aeb583dbee76776
Patrick Morais <ppternunes@gmail.com>
2025-12-03 11:53:29 -0300

    fix(bottom-sheet): internal padding

--- src/app/components/drawer-contents/social-name-drawer-content.tsx
+++ src/app/components/drawer-contents/social-name-drawer-content.tsx
@@ -2,7 +2,7 @@
 
 export function SocialNameDrawerContent() {
   return (
-    <div className="py-6 px-2 flex justify-center">
+    <div className="px-2 flex justify-center">
       <p className="text-popover-foreground">
         {' '}
         Nome social é o nome que você usa no dia a dia. Ele será respeitado e
