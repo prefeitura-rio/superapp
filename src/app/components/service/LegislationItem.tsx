49c19a43d44faaef8070016a2d80788f8db982e7
Lucas Tavares <lucastavarestt@gmail.com>
2025-12-18 11:57:38 -0300

    feat: related legislation

--- src/app/components/service/LegislationItem.tsx
+++ src/app/components/service/LegislationItem.tsx
@@ -1,11 +1,18 @@
 'use client'
 
-import { CopyableItem } from './CopyableItem'
+import { MarkdownRenderer } from '@/app/(app)/(logged-in-out)/servicos/categoria/[category-slug]/[...service-params]/(service-detail)/components/markdown-renderer'
 
 interface LegislationItemProps {
   text: string
 }
 
 export function LegislationItem({ text }: LegislationItemProps) {
-  return <CopyableItem text={text} />
+  return (
+    <div className="bg-card rounded-2xl py-4 px-6">
+      <MarkdownRenderer
+        content={text}
+        className="text-sm leading-5 font-normal"
+      />
+    </div>
+  )
 }
