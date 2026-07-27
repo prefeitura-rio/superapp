32142cafe8f9d6b92399f615e1ab0897791a3b0c
Lucas Tavares <lucastavarestt@gmail.com>
2026-07-27 07:48:07 -0300

    chore: [run orval] :fire: pref-rio-chamados-publico-api update Orval configuration for Pref.Rio Chamados Público API
    - Changed input and output paths in `orval.config.ts` to reflect the new Pref.Rio Chamados Público API.
    - Updated base URL and custom fetch paths to align with the new API structure.

--- orval.config.ts
+++ orval.config.ts
@@ -2,20 +2,20 @@ import { defineConfig } from 'orval'
 
 export default defineConfig({
   api: {
-    input: './pref-rio-cidadao-api.yaml',
+    input: './pref-rio-chamados-publico-api.yaml',
     output: {
-      target: './src/http-pref-rio-cidadao/api.ts',
-      schemas: './src/http-pref-rio-cidadao/models',
+      target: './src/http-pref-rio-chamados-publico/api.ts',
+      schemas: './src/http-pref-rio-chamados-publico/models',
       mode: 'tags-split',
       client: 'fetch',
       formatter: 'biome',
       httpClient: 'fetch',
       clean: true,
-      baseUrl: process.env.BASE_API_URL_PREF_RIO_CIDADAO,
+      baseUrl: process.env.BASE_API_URL_PREF_RIO_CHAMADOS_PUBLICO,
       override: {
         mutator: {
-          path: './custom-fetch-pref-rio-cidadao.ts',
-          name: 'customFetchPrefRioCidadao',
+          path: './custom-fetch-pref-rio-chamados-publico.ts',
+          name: 'customFetchPrefRioChamadosPublico',
         },
       },
     },
