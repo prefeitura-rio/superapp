6168e809401fa4b13fd0c378fb4198aa611bd735
Lucas Tavares <lucastavarestt@gmail.com>
2026-09-17 10:15:25 -0300

    chore: [run orval] :fire: app-rmi

--- orval.config.ts
+++ orval.config.ts
@@ -3,19 +3,19 @@ import { defineConfig } from 'orval'
 export default defineConfig({
   api: {
     input:
-      'https://raw.githubusercontent.com/prefeitura-rio/app-go-api/refs/heads/main/docs/swagger.yaml',
+      'https://raw.githubusercontent.com/prefeitura-rio/app-rmi/refs/heads/main/docs/swagger.json',
     output: {
-      target: './src/http-courses/api.ts',
-      schemas: './src/http-courses/models',
+      target: './src/http/api.ts',
+      schemas: './src/http/models',
       mode: 'tags-split',
       client: 'fetch',
       formatter: 'biome',
       httpClient: 'fetch',
       clean: true,
-      baseUrl: process.env.COURSES_BASE_API_URL,
+      baseUrl: process.env.NEXT_PUBLIC_RMI_BASE_API_URL,
       override: {
         mutator: {
-          path: './custom-fetch-course.ts',
+          path: './custom-fetch.ts',
           name: 'customFetch',
         },
       },
