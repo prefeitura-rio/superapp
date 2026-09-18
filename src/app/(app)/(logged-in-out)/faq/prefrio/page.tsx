b41c1a4e9540bd9c00220c1ca8304eaf370106d8
Patrick Morais <ppternunes@gmail.com>
2026-09-18 10:34:27 -0300

    chore(faq): add force-static export to prefrio and legacy servicos faq pages

--- src/app/(app)/(logged-in-out)/faq/prefrio/page.tsx
+++ src/app/(app)/(logged-in-out)/faq/prefrio/page.tsx
@@ -1,6 +1,8 @@
 import { faqSections } from '@/constants/faqs/pref-rio'
 import { FaqPrefRioClient } from './faq-prefrio-client'
 
+export const dynamic = 'force-static'
+
 export default function FaqPagePrefRio() {
   return <FaqPrefRioClient sections={faqSections} />
 }
--- src/app/(app)/(logged-in-out)/servicos/(servicos)/trabalho/faq/page.tsx
+++ src/app/(app)/(logged-in-out)/servicos/(servicos)/trabalho/faq/page.tsx
@@ -1,6 +1,8 @@
 import { trabalhoFaqSections } from '@/constants/faqs/trabalho'
 import { FaqTrabalhoClient } from './faq-trabalho-client'
 
+export const dynamic = 'force-static'
+
 export default function EmpregosFaqPage() {
   return <FaqTrabalhoClient sections={trabalhoFaqSections} />
 }
